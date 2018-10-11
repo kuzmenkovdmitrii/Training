64eb2cf3f6598a0b0a333beaf13366bb07e3cccf
kuzmenkovdmitrii <kuzmenkovdmitrii@mail.ru>
2018-10-11 19:22:20 +0300

    Added first task for home work number 6. inheritance

---
+++ hw6/task1/task1/Teacher.cs
@@ -0,0 +1,24 @@
+﻿namespace task1
+{
+    class Teacher : Man
+    {
+        public string Status { get; set; }
+        public int Experience { get; set; }
+
+        public Teacher(string name, int age, string gender, double weight, string status, int experience) : base(name, age, gender, weight)
+        {
+            Status = status;
+            Experience = experience;
+        }
+
+        public void IncreaseExperience(int increaseValue)
+        {
+            Experience++;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "|Status: " + Status + "|Experience: " + Experience;
+        }
+    }
+}
